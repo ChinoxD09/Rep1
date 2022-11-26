c0b060d72745646562929888e355b09e969c2374
unknown <ChinoxD09>
2022-11-26 17:19:25 -0600

    Se edita el Codigo.c para que los demas compañeros editen

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+
+	/* Uso de la sentencia scanf() */
+
+	#include <stdio.h>
+
+	main() /* Solicita dos datos */
+	{
+		printf("Hello World\n");
+		
+		char nombre[10];
+		int numero;
+		
+		
+		printf("Introduce tu nombre: ");
+		scanf("%s",nombre);
+		printf("Desea continuar?(y=1/n=0):");
+		scanf("%d",&numero);
+		
+	
+	}
